cc3b4ca6dbe182ee7547e1a28d16409036cd6237
TuxCoder <git@o-g.at>
2014-12-30 02:42:53 +0100

    awesome

--- ProgAdvanced.php
+++ ProgAdvanced.php
@@ -31,26 +31,42 @@ class ProgAdvanced {
         $spot->addSignal(new SignalRectangel(8,0.1,[1,1,1,0,0]));
           $spot->addSignal(new SignalRandom(8,7,[1,1,1,0,0]));
       }
-    /*$this->spots[1]->addSignal(new SignalRectangel(0,5,[1,0,0,0,0]));
-    $this->spots[1]->addSignal(new SignalSawtooth(0,5,[1,0,0,0,0]));
-    $this->spots[3]->addSignal(new SignalRamp(2,5));
-   */
-    //$this->spots[4]->addSignal(new SignalRamp(-1,2,[0,1,1,0,0]));
+    $this->spots[0]->addSignal(new SignalRamp(15,3,[0,0,1,0,0]));
+    $this->spots[1]->addSignal(new SignalRectangel(15,3,[0,1,0,0,0]));
+    $this->spots[2]->addSignal(new SignalRectangel(15,3,[1,0,0,0,0]));
+    $this->spots[3]->addSignal(new SignalRectangel(15,3,[0,1,0,0,0]));
+    $this->spots[4]->addSignal(new SignalRamp(15,3,[0,0,1,0,0]));
     
-    //$this->spots[3]->addSignal(new SignalRamp(2,5));
+    $this->spots[0]->addSignal(new SignalRectangel(18,0.5,[0,0,1,0,0]));
+    $this->spots[1]->addSignal(new SignalRectangel(18.5,0.5,[0,1,0,0,0]));
+    $this->spots[2]->addSignal(new SignalRectangel(19,0.5,[1,0,0,0,0]));
+    $this->spots[3]->addSignal(new SignalRectangel(19.5,0.5,[0,1,0,0,0]));
+    $this->spots[4]->addSignal(new SignalRectangel(20,0.5,[0,0,1,0,0]));
+      
+    $this->spots[4]->addSignal(new SignalRectangel(20,0.5,[0,0,1,0,0]));
+    $this->spots[3]->addSignal(new SignalRectangel(21,0.5,[0,1,0,0,0]));
+    $this->spots[2]->addSignal(new SignalRectangel(21.5,0.5,[1,0,0,0,0]));
+    $this->spots[1]->addSignal(new SignalRectangel(22,0.5,[0,1,0,0,0]));
+    $this->spots[0]->addSignal(new SignalRectangel(22.5,0.5,[0,0,1,0,0]));
+      
+      foreach($this->spots as $spot) {
+          $spot->addSignal(new SignalRandom(23,7,[1,1,1,0,0]));
+      }
     
-    $x=0;
+      $start=0;
+      
+    $x=$start;
     while(1) {
       for($i=0;$i<5;$i++) {
         $this->spots[$i]->calc($x);
       }
       
       $this->dmx->send();
-      usleep(100000);
+      usleep(10000);
       
-      $x+=0.5;
-      if($x>15)
-        $x=0;
+      $x+=0.05;
+      if($x>30)
+        $x=$start;
     }
   }
 }
